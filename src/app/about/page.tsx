326aaa2d951d494790bf6a8153381f558d58dd9a
salman amjad <salmanamjad902@gmail.com>
2024-12-07 03:19:04 +0500

    about page added

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import HeroAbout from '@/components/About/HeroAbout'
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div className='w-full h-full bg-gradient-to-r from-[#141E4C] via-[#00000040] to-[#0f1015] '>
+      <HeroAbout/>
+    </div>
+  )
+}
+
+export default AboutPage
