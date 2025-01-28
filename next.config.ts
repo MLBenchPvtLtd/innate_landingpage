3645924195ec9d87d02bef92d00ab09c3a8ae104
abdulrafay23butt <abdulrafay23butt@gmail.com>
2025-01-28 15:50:22 +0500

    changing back next.config to check redirection

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
- 
+  async redirects() {
+    if (process.env.NEXT_PUBLIC_ENV === 'development') {
+      return [];
+    }
+    else {
+      return [
+        {
+          source: '/:path*',
+          destination: 'https://www.innate-nw.com/:path*',
+          permanent: true,
+        },
+      ];
+    }
+  },
 };
 
 export default nextConfig;
