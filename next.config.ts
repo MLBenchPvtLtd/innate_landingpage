50ab7192e4ce533156ce6f77fc4ebae59021c8b4
abdulrafay23butt <abdulrafay23butt@gmail.com>
2025-01-28 15:57:02 +0500

    added .htaccess file

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    if (process.env.NEXT_PUBLIC_ENV === 'development') {
-      return [];
-    }
-    else {
-      return [
-        {
-          source: '/:path*',
-          destination: 'https://www.innate-nw.com/:path*',
-          permanent: true,
-        },
-      ];
-    }
-  },
+ 
 };
 
 export default nextConfig;
