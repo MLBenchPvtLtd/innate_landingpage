63254f32d76c40f78632a262793fbf61a168c1f5
abdulrafay23butt <abdulrafay23butt@gmail.com>
2025-01-30 19:41:37 +0500

    removed next.config content

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,7 @@
 import { NextConfig } from 'next';
 
-// /** @type {import('next').NextConfig} */
-const nextConfig:NextConfig = {
-    async redirects() {
-      if (process.env.NEXT_PUBLIC_ENV === 'development') {
-        return [];
-      } else {
-        return [
-          {
-            source: '/:path*',
-            has: [
-              {
-                type: 'host',
-                value: 'innate-nw.com', // Redirect ONLY if the host is non-www
-              },
-            ],
-            destination: 'https://www.innate-nw.com/:path*',
-            permanent: true,
-          },
-        ];
-      }
-    },
-  };
-  
-  export default nextConfig;
-  
+const nextConfig: NextConfig = {
+ 
+};
+
+export default nextConfig;
