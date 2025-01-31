654140e9604ecded51c19f59d891c9c67281d79a
Arham utt011 <arhamhamidbutt@gmail.com>
2025-01-30 20:43:47 -0800

    Make project page and add new client requirements

--- src/components/Work/Content/Victoria/First.tsx
+++ src/components/Work/Content/Victoria/First.tsx
@@ -38,10 +38,11 @@ const First = () => {
               Bothell, Washington
             </Text>
             <Link
-              href="/services"
+              href="#"
+              onClick={(e) => e.preventDefault()}
               className="mt-10 mob:mt-6 font-normal max-w-[200px] h-[62.69px] mx-auto flex items-center justify-center text-center gap-1 text-[18px] mob:text-[12px] text-white border border-[#FFFFFF] px-[3px] py-[17.34px] mob:max-w-[140px] mob:px-[7px] mob:py-[7.33px] mob:h-[40px]"
             >
-              LEARN MORE{' '}
+              LEARN MORE
               <Image src={arrowwhite} alt="" width={18} height={18} />
             </Link>
           </div>
