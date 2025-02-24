ca1e2416d860a822f9f4c853907dbd8967a01f2c
Developer <developer@innovativemojo.com>
2025-02-25 01:16:06 +0500

    work page done

--- src/components/Work/Content/Bosma/First.tsx
+++ src/components/Work/Content/Bosma/First.tsx
@@ -11,10 +11,14 @@ const First = () => {
     <>
       <div className="gradient relative w-full z-10 mob:h-[100vh] h-screen min-h-[700px] flex justify-center items-center px-20 mob:px-5">
         <video
-          className="fixed top-0 left-0 w-full h-full object-cover z-0"
+          className="absolute top-0 left-0 w-full h-full object-cover z-0"
           autoPlay
           loop
           muted
+          playsInline
+          webkit-playsinline="true"
+          disablePictureInPicture
+          controls={false} 
           onLoadedData={() => {
             setVideoLoaded(true)
             console.log('Video loaded')
--- src/components/Work/WorkSlider1/index.tsx
+++ src/components/Work/WorkSlider1/index.tsx
@@ -48,7 +48,7 @@ function WorkSlider1() {
               >
                 Desert Oasis
               </Text>
-              <Text className="text-[28px] font-normal mob:text-[11.84px] text-[#FFFFFF] mb-3 flex items-center gap-1 text-center justify-center mob:text-[11.84px]">
+              <Text className="text-[28px] font-normal  text-[#FFFFFF] mb-3 flex items-center gap-1 text-center justify-center mob:text-[11.84px]">
                 Palm Springs, California
               </Text>
               <Link
